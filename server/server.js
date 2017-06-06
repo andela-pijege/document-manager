d751b4d77802027e56adc1c95dcba852cf396bc0
andela-pijege <precious.ijege@andela.com>
2017-06-06 10:45:56 +0100

    modify server

--- server/server.js
+++ server/server.js
@@ -1,28 +1,34 @@
+import path from 'path';
+
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const db = require('./models/index');
-const serverRoutes = require('./routes/index')
+const serverRoutes = require('./routes/index');
 // Set up the express app
 const app = express();
 
-const port = parseInt(process.env.PORT, 10) || 8000;
+const port = parseInt(process.env.PORT, 10) || 9000;
 app.set('port', port);
 
 // Log requests to the console.
 app.use(logger('dev'));
+app.use(express.static('client'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/index.html'));
+});
+
 serverRoutes(app);
 
-// Setup a default catch-all route that sends back a welcome message in JSON format.
-
-if(process.env.NODE_ENV !== 'test') {
+if (process.env.NODE_ENV !== 'test') {
   db.sequelize.sync().done(() => {
     app.listen(port, () => {
       console.log(`Started up at port port ${port}`);
-	  });
+    });
   });
 }
 
