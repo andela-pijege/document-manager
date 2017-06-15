40b18ab77910e57b30523579d02aaf7d1fa781df
andela-pijege <precious.ijege@andela.com>
2017-06-15 09:57:15 +0100

    add more routes for the application

--- client/route.jsx
+++ client/route.jsx
@@ -2,10 +2,18 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import Home from './components/Home';
 import App from './components/App';
+import Login from './components/Login';
+import SignUp from './components/Signup';
+import Dashboard from './components/Dashboard';
+import CreateDocument from './components/CreateDocument';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={Home} />
+    <Route path="/login" components={Login} />
+    <Route path="/signUp" components={SignUp} />
+    <Route path="/dashboard" components={Dashboard} />
+    <Route path="/createDocument" components={CreateDocument} />
   </Route>
 );
 
