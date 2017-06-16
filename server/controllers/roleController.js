5397db99e5736c39c9edc225054a0d254358eb5c
andela-pijege <precious.ijege@andela.com>
2017-06-16 17:50:35 +0100

    fix eslint errors

--- server/controllers/roleController.js
+++ server/controllers/roleController.js
@@ -4,9 +4,9 @@ const roleController = {
   create(req, res) {
     return Roles
       .create({
-        title: req.body.title
+        title: req.body.title,
       })
-      .then(() => res.status(201).send({message: 'role created succesfully'}))
+      .then(() => res.status(201).send({ message: 'role created succesfully' }))
       .catch(error => res.status(500).send(error));
   },
 };
