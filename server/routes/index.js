a5aee9960828605997b1adbeadf1e41a8132817b
andela-pijege <precious.ijege@andela.com>
2017-06-16 17:49:13 +0100

    add more routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,49 +1,56 @@
 const UserController = require('../controllers/userController');
 const DocumentController = require('../controllers/documentController');
-// const RoleController = require('../controllers/roleController');
+const RoleController = require('../controllers/roleController');
 
 const Routes = (app) => {
   // app.get('/', (req, res) => res.status(200).send({
   //   message: 'Welcome to the Documento API by Ghost .........',
   // }));
 
+  app.post(
+    '/api/roles', RoleController.create,
+  );
+
   // users routes
   app.post(
-    '/api/users', UserController.create
+    '/api/users', UserController.create,
   );
   app.post(
-    '/api/users/login', UserController.login
+    '/api/users/login', UserController.login,
   );
   app.get(
-    '/api/users', UserController.getAll
+    '/api/users', UserController.getAll,
   );
   app.get(
-    '/api/users/:id', UserController.getOneUser
+    '/api/users/:id', UserController.getOneUser,
   );
   app.put(
-    '/api/users/:id', UserController.update
+    '/api/users/:id', UserController.update,
+  );
+  app.get(
+    '/api/users/:id/documents', UserController.getDocuments,
   );
 
   app.delete(
-    '/api/users/:id', UserController.delete
+    '/api/users/:id', UserController.delete,
   );
 
   // Documentss routes
 
   app.post(
-    '/api/documents', DocumentController.create
+    '/api/documents', DocumentController.create,
   );
   app.get(
-    '/api/documents', DocumentController.getAll
+    '/api/documents', DocumentController.getAll,
   );
   app.get(
-    '/api/documents/:id', DocumentController.getOneDocument
+    '/api/documents/:id', DocumentController.getOneDocument,
   );
   app.put(
-    '/api/documents/:id', DocumentController.update
+    '/api/documents/:id', DocumentController.update,
   );
   app.delete(
-    '/api/documents/:id', DocumentController.delete
+    '/api/documents/:id', DocumentController.delete,
   );
 };
 
