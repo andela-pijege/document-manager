45cece074bacde707c3ebe9251fe01a5bcad8df7
andela-pijege <precious.ijege@andela.com>
2017-06-23 09:08:26 +0100

    include front end authorization

--- client/index.jsx
+++ client/index.jsx
@@ -2,10 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
+import jwtDecode from 'jwt-decode';
+
+import './styles/index.scss';
 import routes from './route';
+import setAuthorizationToken from './utils/authorization';
+import { loginSuccess } from './actions/LoginAction';
 import configureStore from './store/configureStore';
 
 const store = configureStore();
+const token = localStorage.jwtToken;
+
+if (token) {
+  setAuthorizationToken(token);
+  const details = jwtDecode(token);
+  store.dispatch(loginSuccess(details));
+}
 ReactDOM.render(
   <Provider store={store}>
     <Router history={browserHistory} routes={routes} />
