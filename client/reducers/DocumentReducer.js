35ea03bcea08531c52f29d735fcaa85875067702
andela-pijege <precious.ijege@andela.com>
2017-06-26 12:32:30 +0100

    add serach action to reducer

--- client/reducers/DocumentReducer.js
+++ client/reducers/DocumentReducer.js
@@ -16,11 +16,21 @@ const documentReducer = (state = initialState, action) => {
       return Object.assign({}, state, {
         publicDocuments: action.documents,
       });
+    case actionTypes.SEARCH_MY_DOCUMENTS:
+      return Object.assign({}, state, {
+        searchedPersonalDocuments: action.documents,
+      });
+    case actionTypes.SEARCH_PUBLIC_DOCUMENTS:
+      return Object.assign({}, state, {
+        searchedPublicDocuments: action.documents,
+      });
     case actionTypes.UPDATE_DOCUMENT_SUCCESS:
       return state;
     case actionTypes.CREATE_DOCUMENT_SUCCESS:
       return state;
     case actionTypes.DELETE_USER_DOCUMENT:
+      return state;
+    case actionTypes.DELETE_USER_SUCCESS:
       return state;
     default:
       return state;
