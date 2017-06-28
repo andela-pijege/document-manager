0cd0e9f1953325ec9b8b53d3348214b089ea7d2b
andela-pijege <precious.ijege@andela.com>
2017-06-28 08:58:22 +0100

    create validation middleware

--- server/middleware/usersAuthorization.js
+++ server/middleware/usersAuthorization.js
@@ -1,11 +1,16 @@
-// const validateCreateUser = (req, res, next) => {
-//   if (req.body.password.length < 8) {
-//     return res.status(400).json({
-//       message: 'password must be greater than 7 characters',
-//     });
-//   }
-//   next();
-// };
+const validateCreateUser = (req, res, next) => {
+  if (req.body.firstName.length < 2 || req.body.lastName.length < 2) {
+    return res.status(400).send({
+      message: 'please first name and last name must be more than 2 characters',
+    });
+  }
+  if (req.body.password.length < 8) {
+    return res.status(400).json({
+      message: 'password must be greater than 7 characters',
+    });
+  }
+  next();
+};
 
 const adminCheck = (req, res, next) => {
   if (req.decoded.roleID !== 1) {
@@ -35,4 +40,5 @@ export {
   adminCheck,
   ownerCheck,
   isOwnerOrAdmin,
+  validateCreateUser,
 };
