8357b1156fcc75d670c5bc182e21a060dfc695ca
andela-pijege <precious.ijege@andela.com>
2017-06-28 09:00:38 +0100

    add style sheets for toastr and sweetalert

--- client/index.jsx
+++ client/index.jsx
@@ -5,10 +5,12 @@ import { Provider } from 'react-redux';
 import jwtDecode from 'jwt-decode';
 
 import './styles/index.scss';
+import './styles/toastr.scss';
 import routes from './route';
 import setAuthorizationToken from './utils/authorization';
 import { loginSuccess } from './actions/LoginAction';
 import configureStore from './store/configureStore';
+import '../node_modules/sweetalert/dist/sweetalert.css';
 
 const store = configureStore();
 const token = localStorage.jwtToken;
