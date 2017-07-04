9265650b57f6466a0cd84db591ee956ebdfcf59a
andela-pijege <precious.ijege@andela.com>
2017-07-04 16:22:16 +0100

    add jsdoc

--- client/actions/LoginAction.js
+++ client/actions/LoginAction.js
@@ -4,12 +4,23 @@ import toastr from 'toastr';
 import * as actionTypes from './ActionTypes';
 import setAuthorizationToken from '../utils/authorization';
 
-
+/**
+ * @desc logs in a user
+ * @param {object} user user details
+ * @returns {object} actiontype, and payload
+ */
 export const loginSuccess = user =>
   ({ type: actionTypes.LOGIN_SUCCESS, user });
 
 export const loginFailure = () =>
   ({ type: actionTypes.LOGIN_FAILURE });
+
+/**
+ *
+ * @desc logs in a user then saves the
+ *  token in the local storage
+ * @param {object} userInfo object containing user info
+ */
 
 export const login = (userInfo) => {
   return dispatch => axios.post('/api/users/login', userInfo)
@@ -27,11 +38,14 @@ export const login = (userInfo) => {
 
 /**
  * logout - logout Action
- * @return {Function}  dispatch an action
+ * @return {object}  dispatch an action
  */
-export function logout() {
-  return (dispatch) => {
-    localStorage.removeItem('jwtToken');
-    dispatch(loginSuccess({}));
-  };
-}
+
+export const logout = () => {
+  return dispatch => axios.get('/api/users/logout')
+    .then((response) => {
+      localStorage.removeItem('jwtToken');
+      dispatch(loginSuccess({}));
+      toastr.success(response.data.message);
+    });
+};
