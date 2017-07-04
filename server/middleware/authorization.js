cf3982dac85eb3b8b787f978f5fc536261d9d273
andela-pijege <precious.ijege@andela.com>
2017-07-04 17:06:38 +0100

    jsdoc

--- server/middleware/authorization.js
+++ server/middleware/authorization.js
@@ -1,6 +1,13 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = {
+  /**
+   * @desc Authenticates User by JWT Token
+   * @param {object} req - The request sent to the route
+   * @param {object} res - The response sent back
+   * @param {object} next - Move to the next action
+   * @return {object} json response
+   */
   authorize(req, res, next) {
     jwt.verify(req.headers.authorization,
     process.env.SECRET_KEY,
