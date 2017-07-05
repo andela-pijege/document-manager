0545ec3fd13d5c664a612d074e171f042a03acf4
andela-pijege <precious.ijege@andela.com>
2017-07-05 11:13:57 +0100

    code cleanup

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,105 +1,46 @@
-/* global $ */
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
 import { browserHistory } from 'react-router';
-import * as DocumentAction from '../actions/DocumentAction';
-import * as LoginAction from '../actions/LoginAction';
+import { logout } from '../actions/LoginAction';
 
 
-class Nav extends Component {
-
-  constructor() {
-    super();
-    this.state = {
-    };
-    this.handleSigninClick = this.handleSigninClick.bind(this);
-    this.handleSignupClick = this.handleSignupClick.bind(this);
-    this.logout = this.logout.bind(this);
-    this.getAllUsers = this.getAllUsers.bind(this);
-    this.getAllPublicDdocuments = this.getAllPublicDdocuments.bind(this);
-    this.dashboard = this.dashboard.bind(this);
-    this.getAllRoleDdocuments = this.getAllRoleDdocuments.bind(this);
-  }
-
-  handleSigninClick() {
-    browserHistory.push('login');
-  }
-
-  handleSignupClick() {
-    browserHistory.push('signUp');
-  }
-
-  getAllUsers() {
-    browserHistory.push('/allUsers');
-  }
-
-  getAllPublicDdocuments() {
-    browserHistory.push('/publicDocuments');
-  }
-
-  dashboard() {
-    browserHistory.push('/dashboard');
-  }
-
-  getAllRoleDdocuments() {
-    browserHistory.push('/rolesDocument');
-  }
-
-  /**
-   * logout - logout a user out
-   * @param  {type} event the event handler
-   * @return {void} no return or void
-   */
-  logout(event) {
-    event.preventDefault();
-    this.props.actions.logout();
-    browserHistory.push('/');
-  }
-  render() {
-    return (
-      <div className="navbar-fixed">
-        <nav className="blue-grey darken-4">
-          <div className="nav-wrapper">
-            <a className="brand-logo">Documento</a>
-            <ul id="nav-mobile" className="right hide-on-small-only">
-              {this.props.isAuthenticated ?
+export const Nav = ({ logout, user, isAuthenticated }) => (
+  <div className="navbar-fixed">
+    <nav className="blue-grey darken-4">
+      <div className="nav-wrapper">
+        <a className="brand-logo">Documento</a>
+        <ul id="nav-mobile" className="right hide-on-small-only">
+          {isAuthenticated ?
+            <div>
+              <li onClick={() => {
+                logout();
+                browserHistory.push('/');
+              }}
+              ><a>Logout <i className="fa fa-sign-out" aria-hidden="true"></i></a></li>
+              <li onClick={() => browserHistory.push('/publicDocuments')}><a>Public Document</a></li>
+              <li onClick={() => browserHistory.push('/rolesDocument')}><a>Role Document</a></li>
+              <li onClick={() => browserHistory.push('/dashboard')}><a>Dashboard</a></li>
+              {(user.roleID === 1) ?
                 <div>
-                  <li onClick={this.logout}><a>Logout</a></li>
-                  <li onClick={() => { this.getAllPublicDdocuments(); }}><a>Public Document</a></li>
-                  <li onClick={() => { this.getAllRoleDdocuments(); }}><a>Role Document</a></li>
-                  <li onClick={() => { this.dashboard(); }}><a>Dashboard</a></li>
-                  {(this.props.user.roleID === 1) ?
-                    <div>
-                      <li onClick={() => { this.getAllUsers(); }}><a>View All Users</a></li>
-                    </div> : <div />}
-                </div> :
-                <div>
-                  <li onClick={() => { this.handleSignupClick(); }}><a>SignUp</a></li>
-                  <li onClick={() => { this.handleSigninClick(); }}><a>SignIn</a></li>
-                </div>
-              }
-            </ul>
-          </div>
-        </nav>
+                  <li onClick={() => browserHistory.push('/allUsers')}><a>View All Users</a></li>
+                </div> : <div />}
+            </div> :
+            <div>
+              <li onClick={() => browserHistory.push('signUp')}><a>SignUp <i className="fa fa-user-plus" aria-hidden="true"></i></a></li>
+              <li onClick={() => browserHistory.push('login')}><a>SignIn <i className="fa fa-sign-in" aria-hidden="true"></i></a></li>
+            </div>
+          }
+        </ul>
       </div>
-    );
-  }
-}
+    </nav>
+  </div>
+);
 
-function mapStateToProps(state) {
+export function mapStateToProps(state) {
   return {
     user: state.LoginReducer.user,
     isAuthenticated: state.LoginReducer.loginUser,
   };
 }
 
-function mapDispatchToProps(dispatch) {
-  return {
-    actions: bindActionCreators(
-      Object.assign(DocumentAction, LoginAction),
-      dispatch),
-  };
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Nav);
+export default connect(mapStateToProps, { logout })(Nav);
