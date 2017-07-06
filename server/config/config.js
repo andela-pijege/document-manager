109effbd88b537a00ae6669b0ee33517f543548a
andela-pijege <precious.ijege@andela.com>
2017-07-06 10:12:17 +0100

    set test console to false

--- server/config/config.js
+++ server/config/config.js
@@ -8,6 +8,7 @@ const environment = {
   'test': {
     url: process.env.TEST_DATABASE_URL,
     'dialect': 'postgres',
+    logging: false,
   },
   'production': {
     url: process.env.DATABASE_URL,
